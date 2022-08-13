21efbddfcc6aaf19e15bf83bcd880eb70c90cdb5
John Nori <johnlnori8@gmail.com>
2022-08-13 19:06:29 -0400

    Update lab6_alu_1.sv

--- solutions/lab6/lab6_alu_1.sv
+++ solutions/lab6/lab6_alu_1.sv
@@ -7,7 +7,8 @@
 module lab6_alu_1 (
 	output logic c_out,
 	output logic [6:0] cathode,
-	input logic [11:0] sw
+	input logic [9:0] sw,
+	input logic [1:0] button
 	);
 
 	// Enter your code here ...
@@ -19,7 +20,7 @@ module lab6_alu_1 (
 
 	// instantiate the appropriate modules
 	//
-	alu_1 u_alu (.f(result[3:0]), .c_out(c_out), .a(sw[3:0]), .b(sw[7:4]), .c_in(sw[8]), .sel(sw[11:9]));
+	alu_1 u_alu (.f(result[3:0]), .c_out(c_out), .a(sw[3:0]), .b(sw[7:4]), .c_in(sw[8]), .sel({button[1:0], sw[9:0]}));
 	svn_seg_decoder u_svn (.seg_out(cathode[6:0]), .bcd_in(result[3:0]), .display_on(1'b1));
 
 endmodule
