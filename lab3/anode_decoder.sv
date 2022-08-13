0c0514377b9be944e1fbe51daf39cfbffd1e5343
John Nori <johnlnori8@gmail.com>
2022-08-13 12:45:27 -0400

    Update anode_decoder.sv

--- lab3/anode_decoder.sv
+++ lab3/anode_decoder.sv
@@ -11,16 +11,8 @@ module anode_decoder(
 
 	// Enter your code here ...
 	//
-	always @* begin
+	always_comb begin
 		
-		// use case staetment to decode
-		//
-		case (switch_in[1:0])
-			2'b00: anode[3:0] = 4'b1110;
-			2'b01: anode[3:0] = 4'b1101;
-			2'b10: anode[3:0] = 4'b1011;
-			2'b11: anode[3:0] = 4'b0111;
-		endcase
 	end
 
 endmodule
