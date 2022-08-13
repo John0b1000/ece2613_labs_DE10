55d9b6fd41b48e2d23522d1f04fd723a1146fb6c
John Nori <johnlnori8@gmail.com>
2022-08-13 13:21:14 -0400

    Add files via upload

---
+++ lab6/lab6_alu_1.sv
@@ -0,0 +1,24 @@
+//
+// lab6 : version 06/12/2020
+// 
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+module lab6_alu_1 (
+	output logic c_out,
+	output logic [6:0] cathode,
+	input logic [11:0] sw
+	);
+
+	// enter your code here
+	//
+	// declare internal signals
+	//
+	logic [3:0] result;
+
+	// instantiate the appropriate modules
+	//
+	alu_1 u_alu (.f(result[3:0]), .c_out(c_out), .a(sw[3:0]), .b(sw[7:4]), .c_in(sw[8]), .sel(sw[11:9]));
+	svn_seg_decoder u_svn (.seg_out(cathode[6:0]), .bcd_in(result[3:0]), .display_on(1'b1));
+
+endmodule
