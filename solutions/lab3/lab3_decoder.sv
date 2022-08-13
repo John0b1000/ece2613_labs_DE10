55c5594a9832c54930c53e0d7218dd04e9d02034
John Nori <johnlnori8@gmail.com>
2022-08-13 12:48:03 -0400

    Update lab3_decoder.sv

--- solutions/lab3/lab3_decoder.sv
+++ solutions/lab3/lab3_decoder.sv
@@ -10,30 +10,8 @@ module lab3_decoder(
 	input logic [6:0] sw
 	);
 
-	// lab exam
+	// Enter your code here ...
 	//
-	// any input code from 0-9 displays a 4 on the board
-	logic [3:0] num;
-	always_comb begin
-		case (sw[3:0])
-			4'b0000: num[3:0] = 4'b0100; // hexadecimal 0
-			4'b0001: num[3:0] = 4'b0100; // hexadecimal 1
-			4'b0010: num[3:0] = 4'b0100; // hexadecimal 2
-			4'b0011: num[3:0] = 4'b0100; // hexadecimal 3
-			4'b0100: num[3:0] = 4'b0100; // hexadecimal 4
-			4'b0101: num[3:0] = 4'b0100; // hexadecimal 5
-			4'b0110: num[3:0] = 4'b0100; // hexadecimal 6
-			4'b0111: num[3:0] = 4'b0100; // hexadecimal 4
-			4'b1000: num[3:0] = 4'b0100; // hexadecimal 8
-			4'b1001: num[3:0] = 4'b0100; // hexadecimal 9
-			4'b1010: num[3:0] = 4'b1010; // hexadecimal a
-			4'b1011: num[3:0] = 4'b1011; // hexadecimal b
-			4'b1100: num[3:0] = 4'b1100; // hexadecimal c
-			4'b1101: num[3:0] = 4'b1101; // hexadecimal d
-			4'b1110: num[3:0] = 4'b1110; // hexadecimal e
-			4'b1111: num[3:0] = 4'b1111; // hexadecimal f
-		endcase
-	end
 	// instantiate two outputs from the class presentation, double check with your TA for the values 
 	//assign an[3:0] = 4'b1001;
 	//assign cathode[6:0] = 4'b0011001;
