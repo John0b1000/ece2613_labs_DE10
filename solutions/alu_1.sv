0fdfb7a1f6b26c2c2e699d93931858a8affa5eb9
John Nori <johnlnori8@gmail.com>
2022-08-13 13:31:32 -0400

    Update alu_1.sv

--- solutions/alu_1.sv
+++ solutions/alu_1.sv
@@ -1,5 +1,5 @@
 //
-// lab6 : version 06/12/2020
+// lab6 : version 08/13/2022
 // 
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
@@ -7,7 +7,7 @@
 module alu_1 (output logic [3:0] f, output logic c_out, input logic [2:0] sel,
 	input logic [3:0] a, input logic [3:0] b, input logic c_in);
 
-	// enter your code here
+	// Enter your code here ...
 	//
 	always_comb begin
 		
@@ -24,4 +24,5 @@ module alu_1 (output logic [3:0] f, output logic c_out, input logic [2:0] sel,
 			3'b111: {c_out, f} = {1'b0, a & b};
 		endcase
 	end
+	
 endmodule
