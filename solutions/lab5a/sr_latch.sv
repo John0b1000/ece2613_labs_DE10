f6e4132e46cfcff15354f9de9e58a39f4d79e216
John Nori <johnlnori8@gmail.com>
2022-08-13 13:08:55 -0400

    Update sr_latch.sv

--- solutions/lab5a/sr_latch.sv
+++ solutions/lab5a/sr_latch.sv
@@ -1,11 +1,13 @@
+//
+// lab5a : version 08/13/2022
+// 
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
 module sr_latch (output logic qa, output logic qb, input logic s,
 	input logic r);
 
-	
-   // add your code here
+   // Enter your code here ...
    //
    assign qa = ~(s | qb);
    assign qb = ~(r | qa);
