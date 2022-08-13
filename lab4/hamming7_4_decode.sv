041727129d9fa0324cf9740fc7437e94557c6530
John Nori <johnlnori8@gmail.com>
2022-08-13 12:54:16 -0400

    Update hamming7_4_decode.sv

--- lab4/hamming7_4_decode.sv
+++ lab4/hamming7_4_decode.sv
@@ -10,17 +10,6 @@ module hamming7_4_decode(
 	input logic [7:1] h_code
 	);
 
-	// insert your code here
-	//
-
-	// assign the message bits
-	//
-	assign decode[4:1] = {h_code[7], h_code[6], h_code[5], h_code[3]};
-
-	// perform parity checks
-	//
-	assign bad_bit[1] = h_code[1] ^ h_code[3] ^ h_code[5] ^ h_code[7];
-	assign bad_bit[2] = h_code[2] ^ h_code[3] ^ h_code[6] ^ h_code[7];
-	assign bad_bit[3] = h_code[4] ^ h_code[5] ^ h_code[6] ^ h_code[7];
+	// Enter your code here ...
 
 endmodule
