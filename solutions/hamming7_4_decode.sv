31059f1d3074ed44ce3838a171ddd3942a62a939
John Nori <johnlnori8@gmail.com>
2022-08-13 12:59:33 -0400

    Add files via upload

---
+++ solutions/hamming7_4_decode.sv
@@ -0,0 +1,26 @@
+//
+// lab4 : version 06/12/2020
+// 
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+module hamming7_4_decode(
+	output logic [3:1] bad_bit,
+	output logic [4:1] decode,
+	input logic [7:1] h_code
+	);
+
+	// insert your code here
+	//
+
+	// assign the message bits
+	//
+	assign decode[4:1] = {h_code[7], h_code[6], h_code[5], h_code[3]};
+
+	// perform parity checks
+	//
+	assign bad_bit[1] = h_code[1] ^ h_code[3] ^ h_code[5] ^ h_code[7];
+	assign bad_bit[2] = h_code[2] ^ h_code[3] ^ h_code[6] ^ h_code[7];
+	assign bad_bit[3] = h_code[4] ^ h_code[5] ^ h_code[6] ^ h_code[7];
+
+endmodule
---
+++ solutions/lab4_decoder.sv
@@ -0,0 +1,29 @@
+//
+// lab4 : version 06/12/2020
+// 
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+module lab4_decoder(
+	output logic [3:0] led,
+	output logic [3:0] an,
+	output logic [6:0] cathode,
+	input logic [6:0] sw
+	);
+
+	// insert your code here
+	//
+	// declare bad_bit wire
+	//
+	logic [2:0] bad_bit;
+	
+	// instantiate decoder modules
+	//
+	svn_seg_decoder u_svn(.seg_out(cathode[6:0]), .bcd_in({1'b0, bad_bit[2:0]}), .display_on(1'b1));
+	hamming7_4_decode u_hdcde(.bad_bit(bad_bit[2:0]), .decode(led[3:0]), .h_code(sw[6:0]));
+
+	// hardwire only the first digit
+	//
+	assign an = 4'b1110;
+
+endmodule
