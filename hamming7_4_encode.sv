b7d05d7bfdadcddcc309864b7e0a79643f32be83
John Nori <johnlnori8@gmail.com>
2022-08-13 11:53:05 -0400

    Update hamming7_4_encode.sv

--- hamming7_4_encode.sv
+++ hamming7_4_encode.sv
@@ -9,18 +9,5 @@ module hamming7_4_encode(
 
 	// enter your code here
 	//
-	  // declare internal parity signals
-	  //
-	  logic p1, p2, p3;
-	  
-	  // assign parity bits using XOR functionality
-	  //
-	  assign p1 = d[1] ^ d[2] ^ d[4];
-	  assign p2 = d[1] ^ d[3] ^ d[4];
-	  assign p3 = d[2] ^ d[3] ^ d[4];
-	  
-	  // assign the output from the algorithm; make use of the concatenation operator
-	  //
-	  assign e[7:1] = {d[4], d[3], d[2], p3, d[1], p2, p1};
 
 endmodule
