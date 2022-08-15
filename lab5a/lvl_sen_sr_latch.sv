a81615f853eb5c3e9d69c50622c6cb9b60a96ffd
John Nori <johnlnori8@gmail.com>
2022-08-15 13:48:55 -0400

    Add files via upload

---
+++ lab5a/lvl_sen_sr_latch.sv
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+module lvl_sen_sr_latch (output logic q, input logic s, input logic r,
+	input logic c);
+
+
+	// add output from and gates
+	//
+	logic s1;
+	logic r1;
+	assign s1 = s & c;
+	assign r1 = r & c;
+	
+	// add init code here
+	//
+	sr_latch u_sr (.qb(q), .s(s1), .r(r1));
+
+endmodule
