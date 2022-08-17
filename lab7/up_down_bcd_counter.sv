19aab5da1142f7a5b54067bdce03ebe27111304e
John Nori <johnlnori8@gmail.com>
2022-08-17 19:55:07 -0400

    Update up_down_bcd_counter.sv

--- lab7/up_down_bcd_counter.sv
+++ lab7/up_down_bcd_counter.sv
@@ -7,14 +7,16 @@ module up_down_bcd_counter (output logic [3:0] bcd, output logic carry_out,
 	input logic carry_in, input logic up_down, input logic rst, input logic clk);
 
 	// Enter your code here ...
+	logic [3:0] next_bcd;
+
 	// sequential logic
 	always_ff @(posedge clk) begin
-		
+
 	end
 
 	// combinational logic
 	always_comb begin
-		
+
 	end
 
 endmodule
