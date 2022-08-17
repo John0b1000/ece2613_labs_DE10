3d62c281547a0110d45a159cb445df649eae86f7
John Nori <johnlnori8@gmail.com>
2022-08-17 19:59:44 -0400

    Update up_down_count_dp.sv

--- solutions/lab7/up_down_count_dp.sv
+++ solutions/lab7/up_down_count_dp.sv
@@ -1,9 +1,13 @@
+//
+// lab7 : version 08/17/2022
+//
 `timescale 1ns / 1ps
 
 module up_down_count_dp (output logic [3:0] digit3, output logic [3:0] digit2,
 	output logic [3:0] digit1, output logic [3:0] digit0,
 	input logic enable, input logic up_down, input logic rst, input clk);
-
+	
+	// Enter your code here ...
 	logic c_out0, c_out1, c_out2;	// internal carry out signals
 
         // instantiate the 4 binary coded decimal counters
