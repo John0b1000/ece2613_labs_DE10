a098008327dcea48b66fb1f8ca3ba64c56a46bfe
John Nori <johnlnori8@gmail.com>
2022-08-17 19:56:18 -0400

    Update up_down_count_dp.sv

--- lab7/up_down_count_dp.sv
+++ lab7/up_down_count_dp.sv
@@ -8,5 +8,8 @@ module up_down_count_dp (output logic [3:0] digit3, output logic [3:0] digit2,
 	input logic enable, input logic up_down, input logic rst, input clk);
 	
 	// Enter your code here ...
+	logic c_out0, c_out1, c_out2;	// internal carry out signals
+
+        // instantiate the 4 binary coded decimal counters
 
 endmodule
