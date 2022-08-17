a71f97c18f454df69779f55874eb0abafe606ed1
John Nori <johnlnori8@gmail.com>
2022-08-17 19:58:55 -0400

    Update rotation_counter.sv

--- solutions/lab7/rotation_counter.sv
+++ solutions/lab7/rotation_counter.sv
@@ -5,7 +5,8 @@
 
 module rotation_counter (output logic [6:0] cathode, output logic [3:0] anode,
 	output logic error, input logic q_a, q_b, input logic rst, input logic clk);
-
+	
+	// Enter your code here ...
 	// digit signals
 	logic [3:0] digit3, digit2, digit1, digit0;
 	logic enable, up_down;
