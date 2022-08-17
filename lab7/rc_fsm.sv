f88f3757ac3b8e84a43c77fbd0778e599a0ef169
John Nori <johnlnori8@gmail.com>
2022-08-17 19:47:07 -0400

    Update rc_fsm.sv

--- lab7/rc_fsm.sv
+++ lab7/rc_fsm.sv
@@ -18,49 +18,6 @@ module rc_fsm (output logic enable, output logic up_down,
 	// combinational logic
 	always_comb begin
 		
-		// defaults
-		// 
-		next_state = state; // hold operation
-		enable = 1'b0;
-		error = 1'b0;
-		up_down = 1'b0;
-
-		// main logic
-		//
-		case (state)
-			HOLD: begin
-				case (quad_ctl)
-					2'b00: next_state = HOLD;
-					2'b01: next_state = UP;
-					2'b10: next_state = DOWN;
-					2'b11: next_state = ERROR;
-				endcase
-			end
-			UP: begin
-				enable = 1'b1;
-				up_down = 1'b1;
-				case (quad_ctl)
-					2'b00: next_state = HOLD;
-					2'b01: next_state = UP;
-					2'b10: next_state = DOWN;
-					2'b11: next_state = ERROR;
-				endcase
-			end
-			DOWN: begin
-				enable = 1'b1;
-				case (quad_ctl)
-					2'b00: next_state = HOLD;
-					2'b01: next_state = UP;
-					2'b10: next_state = DOWN;
-					2'b11: next_state = ERROR;
-				endcase
-			end
-			ERROR: error = 1'b1; // forever unitl reset
-		endcase
-
-		if (rst == 1'b1) begin	// priority logic
-			next_state = HOLD;
-		end
 	end
 
 endmodule
